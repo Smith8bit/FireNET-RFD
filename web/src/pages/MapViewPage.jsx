e09350670452158918a29df9ca46e4b6d848ad7a
K. Siwatt <thesmithdot@gmail.com>
2026-05-22 15:01:39 +0700

    config Tailwindcss

--- web/src/pages/MapViewPage.jsx
+++ web/src/pages/MapViewPage.jsx
@@ -13,7 +13,9 @@ export default function MapViewPage() {
   return (
     <div>
       <h1>Map View Page</h1>
-      <button onClick={() => send({ link: url })}>
+      <button
+        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        onClick={() => send({ link: url })}>
         Send Message
       </button>
       {/* Add your map view content here */}
