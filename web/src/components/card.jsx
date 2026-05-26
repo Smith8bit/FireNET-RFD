3a4b2c32955476b1c0796cf42ffe622895d80b3c
K. Siwatt <thesmithdot@gmail.com>
2026-05-26 15:36:24 +0700

    card store unique id

--- web/src/components/card.jsx
+++ web/src/components/card.jsx
@@ -3,12 +3,13 @@ import { useEffect } from 'react'
 
 export default function Card({ Title, Type, Date, Time, Officer=0, id}) {
     
-    const isHovered = useHoverStore((s) => s.hoveredMarker?.id === id);
+    const markerId = Date + Time + Title
+    const hoveredTitle = useHoverStore((s) => s.hoveredMarker?.id === id ? markerId : null);
     const setHoveredMarker = useHoverStore((s) => s.setHoveredMarker);
     
     useEffect(() => {
-        console.log(isHovered)
-    }, [isHovered])
+        console.log(hoveredTitle)
+    }, [hoveredTitle])
 
     return (
         <div className="bg-white rounded-lg shadow-md p-4 w-full h-fit mb-4 text-left"
