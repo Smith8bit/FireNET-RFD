45c6ab3ea512b2f7c343c2b1b1fbdb9c6ad0c03e
K. Siwatt <thesmithdot@gmail.com>
2026-06-07 00:16:01 +0700

    Init Expo
    Init Expo, a mobile sector of project.

--- web/src/components/navbar.jsx
+++ web/src/components/navbar.jsx
@@ -1,9 +1,10 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import logo from '../assets/RFD_logo.svg'
 import { useAuthStore } from "../functions/useAuthStore";
 
 export default function Navbar() {
   const navigate = useNavigate()
+  const location = useLocation()
   const user = useAuthStore((s) => s.user)
   const logout = useAuthStore((s) => s.logout)
 
@@ -19,8 +20,8 @@ export default function Navbar() {
   }
 
 return (
-    <nav className="flex justify-between items-center p-2 bg-white font-medium text-md">
-      <div className="flex items-center ml-4 gap-4">
+    <nav className="flex justify-between items-center px-2 bg-white font-medium text-md">
+      <div className="flex items-center ml-4 gap-4 py-2">
         <img
           className="h-12 "
           src={logo}
@@ -33,12 +34,12 @@ return (
       <span className="font-light italic bg-primary-foreground border-2 border-gray-300 rounded-full px-4 py-1">
         {user.email}
       </span>
-      <ul className="flex gap-2 h-full">
+      <ul className="flex h-full">
         {links.map((link) => (
           <li className="h-full " key={link.path}>
             <Link
               to={link.path}
-              className="h-full flex items-center hover:bg-forest-100 hover:text-forest-700 rounded-full px-3"
+              className={`h-full flex items-center px-3 hover:bg-forest-100 hover:text-forest-700 ${location.pathname === link.path ? 'bg-forest-100 text-forest-700' : ''}`}
             >
               {link.name}
             </Link>
@@ -47,7 +48,7 @@ return (
         <button
               type="button"
               onClick={handleLogout}
-              className="mx-2 bg-forest-500 text-white border border-forest-500 rounded-full px-4 py-2 hover:bg-forest-600"
+              className="mx-2 bg-forest-500 text-white border border-forest-500 rounded-full px-4 my-2 hover:bg-forest-600"
             >
             ออกจากระบบ
         </button>
